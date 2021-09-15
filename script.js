73b100de53f0e0e7a4935142d4be6ead3e208baf
luketunnicliffe <luketunnicliffe@gmail.com>
2021-09-15 21:33:32 +0900

    Added menu toggle function

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //Demonstrates use of font-awesome, modals
+
+const toggle = document.getElementById('toggle');
+const close = document.getElementById('close');
+const open = document.getElementById('open');
+const modal = document.getElementById('modal');
+
+
+//Toggle Nav
+
+toggle.addEventListener('click', () => document.body.classList.toggle('show-nav'));
+
+
