ff45e4919dca309225ba1e5198e21f657c74a49a
luketunnicliffe <luketunnicliffe@gmail.com>
2021-09-15 21:41:35 +0900

    Added JS for modals

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//Demonstrates use of font-awesome, modals
+//Demonstrates use of font-awesome, modals and animation CSS
 
 const toggle = document.getElementById('toggle');
 const close = document.getElementById('close');
@@ -10,4 +10,14 @@ const modal = document.getElementById('modal');
 
 toggle.addEventListener('click', () => document.body.classList.toggle('show-nav'));
 
+// Show modal
 
+open.addEventListener('click', () => modal.classList.add('show-modal'));
+
+//hide modal
+
+close.addEventListener('click', () => modal.classList.remove('show-modal'));
+
+//Hide modal on outside click
+
+window.addEventListener('click', e => e.target == modal ? modal.classList.remove('show-modal') : false);
